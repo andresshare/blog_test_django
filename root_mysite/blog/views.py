60b8ad89f291efc6862d553bc7b626791e6de702
andresshare <andresshare@gmail.com>
2018-07-18 23:48:12 -0500

    urls,views , templates_created modified

--- root_mysite/blog/views.py
+++ root_mysite/blog/views.py
@@ -1,4 +1,5 @@
-from django.shorcuts import render
+from django.shorcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from blog.models import Post, Comment
 from blog.forms import PostForm, CommentForm
 from django.urls import reverse_lazy
@@ -42,3 +43,39 @@ class DraftListView(LoginRequiredMixin, List):
     model = Post
     def get_queryset(self):
         return Post.objects.filter(publish_date_isnull=True).order_by('created_date')
+
+
+@Login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish
+    return redirect('post_detail',pk=pk)
+
+
+@Login_required
+def add_comment_to_post(request, pk)
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+        else:
+            form = CommentForm()
+        return render(request, 'blog/comment_form.html', {'form': form})
+
+@Login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@Login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post.pk)
